f0df777f2ea8ee83a2b2d4a318d80b1d5286babb
Yan Lima Barbosa <yanbr763@gmail.com>
2025-02-28 05:58:53 -0300

    feat: add connect calendar page for user registration and navigation to connect calendar after user registration

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,36 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  // async function handleRegister(data) { }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida que em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Connectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit" /* disabled={isSubmitting} */>
+          Próximo passo <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
