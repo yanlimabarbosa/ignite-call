d1721a15a950658c4d83248716f1549f19a62a24
Yan Lima Barbosa <yanbr763@gmail.com>
2025-02-28 04:48:42 -0300

    feat: add user registration API endpoint and integrate with frontend

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,11 +1,12 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
-import { Container, Form, FormError, Header } from './styles'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { api } from '../../lib/axios'
+import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +42,16 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+
+      // router.push('/register/connect-calendar')
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
