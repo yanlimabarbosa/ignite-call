8166a36499cf092243fd9f9bfea7c6de9bae1d71
Yan Lima Barbosa <yanbr763@gmail.com>
2025-03-07 21:27:24 -0300

    feat: add schedule list visual structure

--- src/pages/schedule/[username]/schedule-form/calendar-step/index.page.tsx
+++ src/pages/schedule/[username]/schedule-form/calendar-step/index.page.tsx
@@ -1,10 +1,63 @@
+import { Text } from '@ignite-ui/react'
 import { Calendar } from '../../../../../components/calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export default function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça feira <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>
+              <Text>08:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>09:00</Text>
+            </TimePickerItem>
+            <TimePickerItem disabled>
+              <Text>10:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>11:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>12:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>13:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>15:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>16:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>17:00</Text>
+            </TimePickerItem>
+            <TimePickerItem>
+              <Text>18:00</Text>
+            </TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
